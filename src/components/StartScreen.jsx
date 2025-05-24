4187e2d502183248684cb6202aeb5c656b9f3572
murad-dabbous <moradyousef04@gmail.com>
2025-05-24 20:17:35 +0000

    Adjusting some styles

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,7 +1,7 @@
 const StartScreen = ({ numberOfQuestions, dispatch }) => {
   return (
     <div className="start">
-      <h2>Welcome to The React Quiz</h2>
+      {/* <h2>Welcome to The React Quiz</h2> */}
       <h3>{numberOfQuestions} questions to test your React mastery</h3>
       <button
         className="btn btn-ui"
